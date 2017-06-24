ba96ff695651471139ff79c1ff42cbb5952281fb
Iain Maitland <iain.maitland@skyscanner.net>
2017-06-24 12:27:36 +0100

    Always extract a sync token from sync API response
    When retrieving a page from the sync API the response will contain
    a 'nextPageUrl' if there are multiple pages of results or a 'nextSyncUrl'
    if the current page is the last in the set of results.
    
    ResourceBuilder.build was only handling cases where the result was the
    last page in a set of results (it only returned a SyncPage when it saw a
    'nextSyncUrl' in the response JSON). This meant that for a result set of
    multiple pages ResourceBuilder.build incorrectly returned an Array
    object instead of a SyncPage.
    
    The SyncPage code was also only able to extract a sync token from a
    response that was the last page in a set of results (i.e., it contained
    a 'nextSyncUrl').
    
    This commit adds the property SyncPage.next_page_url and modifies
    SyncPage._get_next_token to extract a sync token from either
    the 'nextPageUrl' or 'nextSyncUrl' (whichever is present).
    ResourceBuilder now checks for both 'nextPageUrl' and 'nextSyncUrl' and
    returns a SyncPage.

---
+++ tests/resource_builder_test.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from contentful.resource_builder import ResourceBuilder
+from contentful.sync_page import SyncPage
+
+
+class ResourceBuilderTest(TestCase):
+    def test__when_build_recieves_a_sync_page__returns_a_SyncPage(self):
+        sync_api_responses = [
+            {
+                "sys": {
+                    "type": "Array"
+                },
+                "items": [],
+                "nextSyncUrl": "https://cdn.contentful.com/spaces/cfexampleapi/sync?sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdybCnV_Cg8OASMKpwo1UY8K8bsKFwqJrw7DDhcKnM2RDOVbDt1E-wo7CnDjChMKKGsK1w5zCrA3CnU7CgEvDtsK6w7B2wrRZwrwPIgDCjVo8PMOoUcK2wqTCl8O1wpY8wpjCkGM"
+            },
+            {
+                "sys": {
+                    "type": "Array"
+                },
+                "items": [],
+                "nextPageUrl": "https://cdn.contentful.com/spaces/cfexampleapi/sync?sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdybCnV_Cg8OASMKpwo1UY8K8bsKFwqJrw7DDhcKnM2RDOVbDt1E-wo7CnDjChMKKGsK1w5zCrA3CnU7CgEvDtsK6w7B2wrRZwrwPIgDCjVo8PMOoUcK2wqTCl8O1wpY8wpjCkGM"
+            }
+        ]
+
+        for response in sync_api_responses:
+            resource_builder = ResourceBuilder(
+                'en-GB',
+                True,
+               response
+            )
+
+            result = resource_builder.build()
+            self.assertTrue(isinstance(result, SyncPage))
+
