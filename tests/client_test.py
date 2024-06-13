8a73bd93dcd79420778dc8aa8073c0b96c8fe414
Bhushan Lodha <bhushanlodha@gmail.com>
2024-06-13 20:39:53 +0530

    decompress response in vcr

--- tests/client_test.py
+++ tests/client_test.py
@@ -53,7 +53,7 @@ class ClientTest(TestCase):
             self.assertEqual(m.call_count, 1)
             self.assertEqual(m.request_history[0].timeout, c.timeout_s)
 
-    @vcr.use_cassette('fixtures/client/content_type_cache.yaml')
+    @vcr.use_cassette('fixtures/client/content_type_cache.yaml', decode_compressed_response=True)
     def test_client_creates_a_content_type_cache(self):
         Client('cfexampleapi', 'b4c0n73n7fu1')
 
@@ -64,28 +64,28 @@ class ClientTest(TestCase):
 
         self.assertFalse(len(ContentTypeCache.__CACHE__) > 0)
 
-    @vcr.use_cassette('fixtures/client/space.yaml')
+    @vcr.use_cassette('fixtures/client/space.yaml', decode_compressed_response=True)
     def test_client_get_space(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         space = client.space()
 
         self.assertEqual(str(space), "<Space[Contentful Example API] id='cfexampleapi'>")
 
-    @vcr.use_cassette('fixtures/client/content_type.yaml')
+    @vcr.use_cassette('fixtures/client/content_type.yaml', decode_compressed_response=True)
     def test_client_get_content_type(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         ct = client.content_type('cat')
 
         self.assertEqual(str(ct), "<ContentType[Cat] id='cat'>")
 
-    @vcr.use_cassette('fixtures/client/content_types.yaml')
+    @vcr.use_cassette('fixtures/client/content_types.yaml', decode_compressed_response=True)
     def test_client_get_content_types(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         cts = client.content_types()
 
         self.assertEqual(str(cts[0]), "<ContentType[City] id='1t9IbcfdCk6m04uISSsaIK'>")
 
-    @vcr.use_cassette('fixtures/client/entry.yaml')
+    @vcr.use_cassette('fixtures/client/entry.yaml', decode_compressed_response=True)
     def test_client_entry(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         entry = client.entry('nyancat')
@@ -93,19 +93,19 @@ class ClientTest(TestCase):
         self.assertEqual(str(entry), "<Entry[cat] id='nyancat'>")
         self.assertEqual(str(entry.best_friend), "<Entry[cat] id='happycat'>")
 
-    @vcr.use_cassette('fixtures/client/entry_not_found.yaml')
+    @vcr.use_cassette('fixtures/client/entry_not_found.yaml', decode_compressed_response=True)
     def test_client_entry_not_found(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         self.assertRaises(EntryNotFoundError, client.entry, 'foobar')
 
-    @vcr.use_cassette('fixtures/client/entries.yaml')
+    @vcr.use_cassette('fixtures/client/entries.yaml', decode_compressed_response=True)
     def test_client_entries(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         entries = client.entries()
 
         self.assertEqual(str(entries[0]), "<Entry[cat] id='happycat'>")
 
-    @vcr.use_cassette('fixtures/client/entries_select.yaml')
+    @vcr.use_cassette('fixtures/client/entries_select.yaml', decode_compressed_response=True)
     def test_client_entries_select(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         entries = client.entries({'content_type': 'cat', 'sys.id': 'nyancat', 'select': ['fields.name']})
@@ -113,14 +113,14 @@ class ClientTest(TestCase):
         self.assertEqual(str(entries[0]), "<Entry[cat] id='nyancat'>")
         self.assertEqual(entries[0].fields(), {'name': 'Nyan Cat'})
 
-    @vcr.use_cassette('fixtures/client/entries_links_to_entry.yaml')
+    @vcr.use_cassette('fixtures/client/entries_links_to_entry.yaml', decode_compressed_response=True)
     def test_client_entries_links_to_entry(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         entries = client.entries({'links_to_entry': 'nyancat'})
         self.assertEqual(len(entries), 1)
         self.assertEqual(str(entries[0]), "<Entry[cat] id='happycat'>")
 
-    @vcr.use_cassette('fixtures/client/entry_incoming_references.yaml')
+    @vcr.use_cassette('fixtures/client/entry_incoming_references.yaml', decode_compressed_response=True)
     def test_entry_incoming_references(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         entry = client.entry('nyancat')
@@ -128,7 +128,7 @@ class ClientTest(TestCase):
         self.assertEqual(len(entries), 1)
         self.assertEqual(str(entries[0]), "<Entry[cat] id='happycat'>")
 
-    @vcr.use_cassette('fixtures/client/entry_incoming_references_with_query.yaml')
+    @vcr.use_cassette('fixtures/client/entry_incoming_references_with_query.yaml', decode_compressed_response=True)
     def test_entry_incoming_references_with_query(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         entry = client.entry('nyancat')
@@ -137,14 +137,14 @@ class ClientTest(TestCase):
         self.assertEqual(str(entries[0]), "<Entry[cat] id='happycat'>")
         self.assertEqual(entries[0].fields(), {'name': 'Happy Cat'})
 
-    @vcr.use_cassette('fixtures/client/entries_links_to_asset.yaml')
+    @vcr.use_cassette('fixtures/client/entries_links_to_asset.yaml', decode_compressed_response=True)
     def test_client_entries_links_to_asset(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         entries = client.entries({'links_to_asset': 'nyancat'})
         self.assertEqual(len(entries), 1)
         self.assertEqual(str(entries[0]), "<Entry[cat] id='nyancat'>")
 
-    @vcr.use_cassette('fixtures/client/asset_incoming_references.yaml')
+    @vcr.use_cassette('fixtures/client/asset_incoming_references.yaml', decode_compressed_response=True)
     def test_asset_incoming_references(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         asset = client.asset('nyancat')
@@ -152,7 +152,7 @@ class ClientTest(TestCase):
         self.assertEqual(len(entries), 1)
         self.assertEqual(str(entries[0]), "<Entry[cat] id='nyancat'>")
 
-    @vcr.use_cassette('fixtures/client/asset.yaml')
+    @vcr.use_cassette('fixtures/client/asset.yaml', decode_compressed_response=True)
     def test_client_asset(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         asset = client.asset('nyancat')
@@ -162,7 +162,7 @@ class ClientTest(TestCase):
             "<Asset id='nyancat' url='//images.contentful.com/cfexampleapi/4gp6taAwW4CmSgumq2ekUm/9da0cd1936871b8d72343e895a00d611/Nyan_cat_250px_frame.png'>"
         )
 
-    @vcr.use_cassette('fixtures/client/locales_on_environment.yaml')
+    @vcr.use_cassette('fixtures/client/locales_on_environment.yaml', decode_compressed_response=True)
     def test_client_locales_on_environment(self):
         client = Client('facgnwwgj5fe', '<ACCESS_TOKEN>', environment='testing', content_type_cache=False)
         locales = client.locales()
@@ -170,7 +170,7 @@ class ClientTest(TestCase):
         self.assertEqual(str(locales), "<Array size='3' total='3' limit='1000' skip='0'>")
         self.assertEqual(str(locales[0]), "<Locale[U.S. English] code='en-US' default=True fallback_code=None optional=False>")
 
-    @vcr.use_cassette('fixtures/client/assets.yaml')
+    @vcr.use_cassette('fixtures/client/assets.yaml', decode_compressed_response=True)
     def test_client_assets(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         assets = client.assets()
@@ -180,7 +180,7 @@ class ClientTest(TestCase):
             "<Asset id='1x0xpXu4pSGS4OukSyWGUK' url='//images.contentful.com/cfexampleapi/1x0xpXu4pSGS4OukSyWGUK/cc1239c6385428ef26f4180190532818/doge.jpg'>"
         )
 
-    @vcr.use_cassette('fixtures/client/sync.yaml')
+    @vcr.use_cassette('fixtures/client/sync.yaml', decode_compressed_response=True)
     def test_client_sync(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         sync = client.sync({'initial': True})
@@ -193,14 +193,14 @@ class ClientTest(TestCase):
         )
         self.assertEqual(str(sync.items[0]), "<Entry[1t9IbcfdCk6m04uISSsaIK] id='5ETMRzkl9KM4omyMwKAOki'>")
 
-    @vcr.use_cassette('fixtures/client/sync_environments.yaml')
+    @vcr.use_cassette('fixtures/client/sync_environments.yaml', decode_compressed_response=True)
     def test_client_sync_with_environments(self):
         client = Client('a22o2qgm356c', 'bfbc63cf745a037125dbcc64f716a9a0e9d091df1a79e84920b890f87a6e7ab9', environment='staging', content_type_cache=False)
         sync = client.sync({'initial': True})
 
         self.assertEqual(sync.items[0].environment.id, 'staging')
 
-    @vcr.use_cassette('fixtures/client/array_endpoints.yaml')
+    @vcr.use_cassette('fixtures/client/array_endpoints.yaml', decode_compressed_response=True)
     def test_client_creates_wrapped_arrays(self):
         client = Client('cfexampleapi', 'b4c0n73n7fu1', content_type_cache=False)
         self.assertEqual(str(client.content_types()), "<Array size='4' total='4' limit='100' skip='0'>")
@@ -341,19 +341,19 @@ class ClientTest(TestCase):
 
     # Integration Tests
 
-    @vcr.use_cassette('fixtures/integration/issue-4.yaml')
+    @vcr.use_cassette('fixtures/integration/issue-4.yaml', decode_compressed_response=True)
     def test_entries_dont_fail_with_unicode_characters(self):
         client = Client('wltm0euukdog', 'bbe871957bb60f988af6cbeeccbb178c36cae09e36e8098357e27b51dd38d88e', content_type_cache=True)
         entries = client.entries()
         self.assertEqual(entries[0].name, '😅')
 
-    @vcr.use_cassette('fixtures/integration/json-arrays.yaml')
+    @vcr.use_cassette('fixtures/integration/json-arrays.yaml', decode_compressed_response=True)
     def test_entries_dont_fail_with_arrays_as_json_root(self):
         client = Client('4int1zgmkwcf', 'd2ac2076019bd4a8357811cbdd5563bb7186d90d77e53c265a1bafd9f83439e8', content_type_cache=True)
         entries = client.entries()
         self.assertEqual(entries[0].json, [{'foo': 'bar'}, {'baz': 'qux'}])
 
-    @vcr.use_cassette('fixtures/integration/issue-11.yaml')
+    @vcr.use_cassette('fixtures/integration/issue-11.yaml', decode_compressed_response=True)
     def test_entries_with_none_values_on_all_fields(self):
         client = Client('rtx5c7z0zbas', 'a6c8dc438d470c51d1094dad146a1f20fcdba41e21f4e263af6c3f70d8583634', content_type_cache=True)
         entry = client.entries()[0]
@@ -368,7 +368,7 @@ class ClientTest(TestCase):
         self.assertEqual(entry.json, None)
         self.assertEqual(entry.link, None)
 
-    @vcr.use_cassette('fixtures/integration/circular-references.yaml')
+    @vcr.use_cassette('fixtures/integration/circular-references.yaml', decode_compressed_response=True)
     def test_circular_references_default_depth(self):
         client = Client('rk19fq93y3vw', '821aa502a7ce820e46adb30fa6942889619aac4342a7021cfe15197c52a593cc', content_type_cache=True)
         a = client.entry('6kdfS7uMs8owuEIoSaOcQk')
@@ -377,7 +377,7 @@ class ClientTest(TestCase):
         self.assertEqual(str(a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a), "<Entry[a] id='6kdfS7uMs8owuEIoSaOcQk'>")
         self.assertEqual(str(a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b), "<Link[Entry] id='7oADpDPuneEAsWUiO2CmEo'>")
 
-    @vcr.use_cassette('fixtures/integration/circular-references.yaml')
+    @vcr.use_cassette('fixtures/integration/circular-references.yaml', decode_compressed_response=True)
     def test_circular_references_set_depth(self):
         client = Client(
             'rk19fq93y3vw',
@@ -390,7 +390,7 @@ class ClientTest(TestCase):
         self.assertEqual(str(a.b), "<Entry[b] id='7oADpDPuneEAsWUiO2CmEo'>")
         self.assertEqual(str(a.b.a), "<Link[Entry] id='6kdfS7uMs8owuEIoSaOcQk'>")
 
-    @vcr.use_cassette('fixtures/integration/circular-references.yaml')
+    @vcr.use_cassette('fixtures/integration/circular-references.yaml', decode_compressed_response=True)
     def test_circular_references_with_reusable_entries(self):
         client = Client('rk19fq93y3vw', '821aa502a7ce820e46adb30fa6942889619aac4342a7021cfe15197c52a593cc', content_type_cache=True, reuse_entries=True)
         a = client.entry('6kdfS7uMs8owuEIoSaOcQk')
@@ -402,7 +402,7 @@ class ClientTest(TestCase):
         self.assertEqual(a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a._depth, 0)
         self.assertEqual(str(a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a.b.a), "<Entry[a] id='6kdfS7uMs8owuEIoSaOcQk'>")
 
-    @vcr.use_cassette('fixtures/integration/errors-filtered.yaml')
+    @vcr.use_cassette('fixtures/integration/errors-filtered.yaml', decode_compressed_response=True)
     def test_unresolvable_entries_dont_get_included(self):
         client = Client(
             '011npgaszg5o',
@@ -412,7 +412,7 @@ class ClientTest(TestCase):
         entry = client.entry('1HR1QvURo4MoSqO0eqmUeO')
         self.assertEqual(len(entry.modules), 2)
 
-    @vcr.use_cassette('fixtures/fields/rich_text.yaml')
+    @vcr.use_cassette('fixtures/fields/rich_text.yaml', decode_compressed_response=True)
     def test_rich_text_field(self):
         client = Client(
             'jd7yc4wnatx3',
@@ -432,7 +432,7 @@ class ClientTest(TestCase):
                 embedded_entry_index += 1
         self.assertEqual(expected_entry_occurrances, 0)
 
-    @vcr.use_cassette('fixtures/fields/rich_text_lists_with_embeds.yaml')
+    @vcr.use_cassette('fixtures/fields/rich_text_lists_with_embeds.yaml', decode_compressed_response=True)
     def test_rich_text_field_with_embeds_in_lists(self):
         client = Client(
             'jd7yc4wnatx3',
@@ -466,7 +466,7 @@ class ClientTest(TestCase):
         self.assertEqual(entry.body['content'][4]['nodeType'], 'ordered-list')
         self.assertEqual(str(entry.body['content'][4]['content'][2]['content'][0]['data']['target']), "<Entry[embedded] id='5ZF9Q4K6iWSYIU2OUs0UaQ'>")
 
-    @vcr.use_cassette('fixtures/integration/issue-41.yaml')
+    @vcr.use_cassette('fixtures/integration/issue-41.yaml', decode_compressed_response=True)
     def test_rich_text_fields_should_not_get_hydrated_twice(self):
         client = Client(
             'fds721b88p6b',
