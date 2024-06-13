b347bab779ce0bfb77a82bf2ee54d5814837719c
Bhushan Lodha <bhushanlodha@gmail.com>
2024-06-13 21:16:44 +0530

    updated setup to list py versions

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-
+        'Programming Language :: Python :: 3.12',
     ],
     test_suite='tests',
     tests_require=test_requirements
